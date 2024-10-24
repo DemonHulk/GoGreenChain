426a4912d7512d071c00bedfacabd335365cdb88
Hulkdemon <al01-077-0321@utdelacosta.edu.mx>
2024-10-24 13:46:33 -0700

    Update 2019_08_19_000000_create_failed_jobs_table.php

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -26,9 +26,6 @@ return new class extends Migration
      * Reverse the migrations.
      */
 
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('failed_jobs');
