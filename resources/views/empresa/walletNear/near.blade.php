00fd28a1a25b5e6ef3d1ee74196e36a976a18041
FabianGomezB <al05-016-0321@utdelacosta.edu.mx>
2024-10-28 20:42:43 -0700

    Ya jala todo

--- resources/views/empresa/walletNear/near.blade.php
+++ resources/views/empresa/walletNear/near.blade.php
@@ -1,23 +1,56 @@
+@extends('adminlte::page')
 
-@section('css')
+@section('title', 'GoGreenChain')
+
+@section('content_header')
 @stop
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NEAR Send</title>
+
+@section('content')
     <script src="https://cdn.jsdelivr.net/npm/near-api-js@2.1.3/dist/near-api-js.min.js"></script>
-</head>
-<body>
-    <button id="login-button">Iniciar Sesión con NEAR</button>
+
+    <div class="card card-primary card-outline my-4">
+        <div class="card-header">
+            <h3 class="card-title">
+                <i class="fas fa-wallet mr-2"></i>
+                Cartera Wallet Empresa
+            </h3>
+        </div>
+        <div class="card-body">
+            <!-- Balance Amount -->
+            <div class="text-center mb-5">
+                <h2 class="display-4 font-weight-bold mb-4 sinsesion"> INICIA SESIÓN </h2>
+                <h2 class="display-4 font-weight-bold mb-4 sesion"> <span id="cuenta"></span> </h2>
+                <h2 class="display-4 font-weight-bold mb-4 sesion"> <span id="balance"></span> NEAR </h2>
+
+                <p class="text-muted sesion">
+                    Balance disponible
+                    <i class="fas fa-info-circle" data-toggle="tooltip" title="Balance actual en su cuenta"></i>
+                </p>
+            </div>
+
+            <!-- Action Buttons -->
+            <div class="row">
+                <div class="col-md-12 sinsesion">
+                    <div class="text-center mb-3">
+                        <button id="login-button" class="btn btn-success btn-lg btn-block login-button">
+                            Iniciar sesión
+                        </button>
+                    </div>
+                </div>
+                <div class="col-md-12 sesion">
+                    <div class="text-center mb-3">
+                        <button id="logout-button" class="btn btn-success btn-lg btn-block logout-button">
+                            Cerrar sesión
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <button id="send-near-button">Enviar NEAR</button>
-    <button id="view-contract-button">Ver Contrato</button>
-    <button id="call-contract-button">Llamar Contrato</button>
-    <button id="get-logs-button">Obtener Logs</button>
-    <button id="logout-button" style="display:none;">Cerrar Sesión</button>
-    <div id="status"></div>
-    <div id="logs"></div>
+    {{-- <button id="get-payments-button">Obtener Pagos</button> --}}
+    <button id="send-payment-button">Enviar Pago</button>
+    <div id="payments"></div>
     <script>
         const config = {
             networkId: 'testnet',
@@ -51,7 +84,8 @@
                 console.log('Resultado de la transacción:', result);
 
                 // Mostrar mensaje de éxito al usuario
-                document.getElementById('status').textContent = 'Transacción realizada con éxito. ID de la transacción: ' + result.transaction_outcome.id;
+                document.getElementById('status').textContent =
+                    'Transacción realizada con éxito. ID de la transacción: ' + result.transaction_outcome.id;
 
                 // Enviar detalles de la transacción al backend
                 const response = await fetch('/log-transaction', {
@@ -76,67 +110,125 @@
             }
         }
 
-        async function viewContract(wallet, contractId, methodName, args) {
+        async function sendPayment(wallet) {
             const account = wallet.account();
-            try {
-                const result = await account.viewFunction(contractId, methodName, args);
-                console.log('Resultado del contrato:', result);
-                document.getElementById('status').textContent = 'Resultado del contrato: ' + JSON.stringify(result);
-            } catch (error) {
-                console.error('Error al ver el contrato:', error);
-                document.getElementById('status').textContent = 'Error al ver el contrato: ' + error.message;
-            }
-        }
+            const cuenta = prompt('A quien enviar:');
+            const tarea = prompt('Porque:');
+            const cantidad = prompt('cuanto:');
 
-        async function callContract(wallet, contractId, methodName, args, gas = '30000000000000', deposit = '0') {
-            const account = wallet.account();
             try {
                 const result = await account.functionCall({
-                    contractId,
-                    methodName,
-                    args,
-                    gas,
-                    attachedDeposit: nearApi.utils.format.parseNearAmount(deposit)
+                    contractId: 'paymentchicha.testnet',
+                    methodName: 'pay',
+                    args: {
+                        recipient: cuenta,
+                        mensaje: tarea
+                    },
+                    gas: '30000000000000',
+                    attachedDeposit: nearApi.utils.format.parseNearAmount(cantidad)
                 });
                 console.log('Resultado de la llamada al contrato:', result);
-                document.getElementById('status').textContent = 'Llamada al contrato realizada con éxito. ID de la transacción: ' + result.transaction_outcome.id;
+                document.getElementById('status').textContent = 'Pago realizado con éxito. ID de la transacción: ' + result.transaction_outcome.id;
             } catch (error) {
-                console.error('Error al llamar al contrato:', error);
-                document.getElementById('status').textContent = 'Error al llamar al contrato: ' + error.message;
+                console.error('Error al realizar el pago:', error);
+                document.getElementById('status').textContent = 'Error al realizar el pago: ' + error.message;
             }
         }
 
-        async function getContractLogs(wallet, contractId) {
-            const account = wallet.account();
+        async function getPayments(wallet) {
+            const { network } = config;
+            const provider = new nearApi.providers.JsonRpcProvider('https://rpc.testnet.near.org');
+
             try {
-                const result = await account.viewFunction(contractId, 'get_messages', {});
-                console.log('Logs del contrato:', result);
-                document.getElementById('logs').textContent = 'Logs del contrato: ' + JSON.stringify(result);
+                const result = await provider.query({
+                    request_type: 'call_function',
+                    account_id: 'paymentchicha.testnet',
+                    method_name: 'get_payments',
+                    args_base64: Buffer.from(JSON.stringify({ from_index: 0, limit: 10 })).toString('base64'),
+                    finality: 'optimistic',
+                });
+
+                const payments = JSON.parse(Buffer.from(result.result).toString());
+
+                // Formatear los datos
+                const formattedPayments = payments.map(payment => {
+                    const date = new Date(Number(payment.timestamp) / 1e6); // Convertir timestamp a milisegundos
+                    const formattedDate = date.toLocaleString(); // Formatear la fecha
+                    const formattedAmount = nearApi.utils.format.formatNearAmount(payment.amount); // Formatear el amount
+                    return {
+                        ...payment,
+                        timestamp: formattedDate,
+                        amount: formattedAmount
+                    };
+                });
+
+                // Crear la tabla HTML
+                let tableHtml = '<table class="table table-striped">';
+                tableHtml += '<thead><tr><th>Realizó pago</th><th>Recibió pago</th><th>Cantidad(N)</th><th>Fecha</th><th>Tarea</th></tr></thead>';
+                tableHtml += '<tbody>';
+                formattedPayments.forEach(payment => {
+                    tableHtml += `<tr>
+                        <td>${payment.sender}</td>
+                        <td>${payment.recipient}</td>
+                        <td>${payment.amount}</td>
+                        <td>${payment.timestamp}</td>
+                        <td>${payment.mensaje}</td>
+                    </tr>`;
+                });
+                tableHtml += '</tbody></table>';
+
+                document.getElementById('payments').innerHTML = tableHtml;
             } catch (error) {
-                console.error('Error al obtener los logs del contrato:', error);
-                document.getElementById('logs').textContent = 'Error al obtener los logs del contrato: ' + error.message;
+                console.error('Error al obtener los pagos del contrato:', error);
+                document.getElementById('payments').textContent = 'Error al obtener los pagos del contrato: ' + error.message;
             }
         }
 
-        function updateUI(wallet) {
+        async function updateUI(wallet) {
+            // Obtén todos los elementos con la clase 'mi-clase'
+            var sesion = document.getElementsByClassName('sesion');
+            var sinsesion = document.getElementsByClassName('sinsesion');
+
             const loginButton = document.getElementById('login-button');
             const logoutButton = document.getElementById('logout-button');
             const status = document.getElementById('status');
 
             if (wallet.isSignedIn()) {
                 loginButton.style.display = 'none';
+                for (var i = 0; i < sesion.length; i++) {
+                    sesion[i].style.display = 'block';
+                }
+
+                for (var i = 0; i < sinsesion.length; i++) {
+                    sinsesion[i].style.display = 'none';
+                }
+
                 logoutButton.style.display = 'block';
-                status.textContent = `Conectado como: ${wallet.getAccountId()}`;
-                // Enviar información de la billetera al backend
-                fetch('/auth/save-wallet', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                    },
-                    body: JSON.stringify({ accountId: wallet.getAccountId() })
-                });
+                const account = wallet.account();
+                const balance = await account.getAccountBalance();
+                const availableBalance = nearApi.utils.format.formatNearAmount(balance.available);
+
+                document.getElementById('cuenta').textContent = `Wallet: ${wallet.getAccountId()}`;
+                document.getElementById('balance').textContent = `Disponible: ${availableBalance}`;
+
+                // // Enviar información de la billetera al backend
+                // fetch('/auth/save-wallet', {
+                //     method: 'POST',
+                //     headers: {
+                //         'Content-Type': 'application/json',
+                //         'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                //     },
+                //     body: JSON.stringify({ accountId: wallet.getAccountId() })
+                // });
             } else {
+                for (var i = 0; i < sesion.length; i++) {
+                    sesion[i].style.display = 'none';
+                }
+
+                for (var i = 0; i < sinsesion.length; i++) {
+                    sinsesion[i].style.display = 'block';
+                }
+
                 loginButton.style.display = 'block';
                 logoutButton.style.display = 'none';
                 status.textContent = 'No has iniciado sesión';
@@ -165,31 +257,18 @@
                 await sendNear(wallet, receiverAccountId, amount);
             };
 
-            document.getElementById('view-contract-button').onclick = async () => {
-                const contractId = prompt('Ingrese el ID del contrato:');
-                const methodName = prompt('Ingrese el nombre del método de vista:');
-                const args = JSON.parse(prompt('Ingrese los argumentos en formato JSON:'));
-                await viewContract(wallet, contractId, methodName, args);
+            document.getElementById('send-payment-button').onclick = async () => {
+                await sendPayment(wallet);
             };
 
-            document.getElementById('call-contract-button').onclick = async () => {
-                const contractId = prompt('Ingrese el ID del contrato:');
-                const methodName = prompt('Ingrese el nombre del método de cambio:');
-                const args = JSON.parse(prompt('Ingrese los argumentos en formato JSON:'));
-                const gas = prompt('Ingrese la cantidad de gas (opcional):', '30000000000000');
-                const deposit = prompt('Ingrese la cantidad de NEAR a depositar (opcional):', '0');
-                await callContract(wallet, contractId, methodName, args, gas, deposit);
-            };
-
-            document.getElementById('get-logs-button').onclick = async () => {
-                const contractId = prompt('Ingrese el ID del contrato:');
-                await getContractLogs(wallet, contractId);
-            };
+            // document.getElementById('get-payments-button').onclick = async () => {
+                await getPayments(wallet);
+            // };
 
             updateUI(wallet);
         }
 
         window.onload = initApp;
     </script>
-</body>
-</html>
+
+@stop
